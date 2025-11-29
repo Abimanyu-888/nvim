c83920f767b2df5616b6413f2ab882cc9785afa6
Abimanyu-888 <abhimanyuchoudhary888@gmail.com>
2025-11-29 12:00:46 +0530

    initial commit

---
+++ init.lua
@@ -0,0 +1,3 @@
+require("dog")
+require("dog.remap")
+require("dog.packer")
---
+++ lua/dog/remap.lua
@@ -0,0 +1,2 @@
+vim.g.mapleader=" " -- implies leader is a variable and equals " "
+vim.keymap.set("n","<leader>pv",vim.cmd.Ex) -- in normal mode remap " pv" to Ex which is go toa file dir
